1e265c09975c1aa36f150535b7edf0e6590d0b1d
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2024-07-31 14:45:53 +0530

    Time: 19 ms (99.13%), Space: 12.6 MB (28.65%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        currentMax = 0
+        currentString = ""
+        
+        for i in range(len(s)):
+            # If the character is new, add to our current substring
+            if s[i] not in currentString:
+                currentString += s[i]
+                if len(currentString) > currentMax:
+                    currentMax = len(currentString)
+            # If the character is not new, slice away the old character and anything before it,
+            # and then add the character to our current substring 
+            else:
+                oldCharPosition = currentString.find(s[i])
+                currentString = currentString[oldCharPosition + 1:] + s[i]
+                        
+        return currentMax
