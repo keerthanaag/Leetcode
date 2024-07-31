4b18dc7092dcdb199a8c2f95ac7fe02b16101d56
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2024-07-31 22:13:05 +0530

    Time: 114 ms (28.23%), Space: 13.9 MB (45.61%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -5,9 +5,20 @@ class Solution(object):
         :rtype: int
         """
         #find the length of the list to know the no of iterations then use count function to find the element which appeared only once
-        n=len(nums)
-        for i in range(0,n):
-            j=nums[i]
-            target=nums.count(j)
-            if target == 1:
-                return j
+        # n=len(nums)
+        # for i in range(0,n):
+        #     j=nums[i]
+        #     target=nums.count(j)
+        #     if target == 1:
+        #         return j
+        d={}
+        for x in nums:
+            if x in d:
+                val=d[x]
+                val+=1
+            else:
+                val=1
+            d.update({x:val})
+        for x in d.keys():
+            if d[x]==1:
+                return x
