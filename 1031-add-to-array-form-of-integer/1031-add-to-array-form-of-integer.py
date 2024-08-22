148a8e8858e6f60d7808f3d7889f75cd63e59afa
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2024-08-22 21:30:04 +0530

    Time: 4734 ms (5.19%), Space: 11.9 MB (76.75%) - LeetHub

--- 1031-add-to-array-form-of-integer/1031-add-to-array-form-of-integer.py
+++ 1031-add-to-array-form-of-integer/1031-add-to-array-form-of-integer.py
@@ -1,5 +1,19 @@
-class Solution:
-    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+class Solution(object):
+    def addToArrayForm(self, num, k):
+        """
+        :type num: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        ans=0
+        for i,x in enumerate(num):
+            ans+=x*(10**(len(num)-i-1))
+        ans+=k
+        print(ans)
+        lst=[]
+        for x in str(ans):
+            lst.append(int(x))
+        return lst
         carry=0
         for i in range(-1,-len(num)-1,-1):
             if k >0:
@@ -25,4 +39,5 @@ class Solution:
         if carry>0:
             num.insert(0,carry)
         return num
+        
         
