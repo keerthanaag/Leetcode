5e5a0e08fd4d32bd6eecb3f7d6a62933462b363d
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2024-08-27 10:46:15 +0530

    Time: 59 ms (5.48%), Space: 16.5 MB (37.95%) - LeetHub

---
+++ 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,23 @@
+from collections import deque
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        d=deque()
+        d.extend(students)
+        while(1):
+            flag=0
+            i=0
+            while i < len(students):
+                if students[0] == sandwiches[0]:
+                    flag=1
+                    students.pop(0)
+                    sandwiches.pop(0)
+                    print("inside if",students,sandwiches)
+                else:
+                    students.append(students.pop(0))
+                print(students,sandwiches)
+                i+=1
+            if flag==0:
+                break
+        return len(students)
+            
+                
