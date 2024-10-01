27b9d568ac87e95b51c2e2167a3ac284eea54476
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2024-10-01 23:10:26 +0530

    Time: 2585 ms (14.63%), Space: 12.5 MB (73.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -6,6 +6,14 @@ class Solution(object):
         :rtype: List[int]
         """
 
+        # map = {}
+        # for i in range(len(nums)):
+        #     if nums[i] not in map:
+        #         map[target - nums[i]] = i 
+        #     else:
+        #         return map[nums[i]], i
+
+        # return -1,-1
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if i!=j and target == nums[i]+nums[j]:
