dd68ae7d7c4a1a288ca53d67738d8b852599c245
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2024-11-06 22:45:10 +0530

    Time: 0 ms (100%), Space: 16.6 MB (54.49%) - LeetHub

---
+++ 2528-number-of-valid-clock-times/2528-number-of-valid-clock-times.py
@@ -0,0 +1,29 @@
+class Solution:
+    def countTime(self, time: str) -> int:
+        ans=1
+        if time[4]=='?':
+            if time[3] == '6':
+                ans=1
+            elif time[3] == '?':
+                ans*=60
+            else:
+                ans*= 10
+        if time[3] == '?' and time[4] != '?':
+            ans*=6
+        if time[1] =='?':
+            if time[0] == '2':
+                ans *= 4
+            elif time[0] == '?':
+                ans *= 24
+            else:
+                ans *= 10
+        if time[0] == '?' and time[1] != '?':
+            if int(time[1]) <= 3:
+                ans*= 3
+            else:
+                ans *= 2
+        return ans
+
+
+
+        
