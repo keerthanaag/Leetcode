a98941610bf3c12701499a5cfdf0b30b9cd2aa8b
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2024-12-02 19:49:29 +0530

    Time: 0 ms (100%), Space: 12.1 MB (5.26%) - LeetHub

--- 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.py
+++ 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.py
@@ -1,5 +1,10 @@
-class Solution:
-    def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
+class Solution(object):
+    def checkAlmostEquivalent(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: bool
+        """
         s=set(word1).union(set(word2))
         for x in s:
             if abs(word1.count(x) - word2.count(x)) >3:
