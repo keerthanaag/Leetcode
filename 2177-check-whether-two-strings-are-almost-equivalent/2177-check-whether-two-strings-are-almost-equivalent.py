1f23e660ca44042bed7b8a8b9f3cea6bf36e0be9
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2024-12-02 19:37:44 +0530

    Time: 3 ms (33.33%), Space: 17.2 MB (5.02%) - LeetHub

--- 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.py
+++ 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.py
@@ -1,7 +1,6 @@
 class Solution:
     def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
         s=set(word1).union(set(word2))
-        print(s)
         for x in s:
             if abs(word1.count(x) - word2.count(x)) >3:
                 return False
