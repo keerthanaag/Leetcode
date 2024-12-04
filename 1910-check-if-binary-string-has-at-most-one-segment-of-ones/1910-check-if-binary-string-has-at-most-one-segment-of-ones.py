4ce0324bd27d24af7cac1b61c916510476ed0965
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2024-12-04 16:46:14 +0530

    Time: 0 ms (100%), Space: 17.1 MB (5.01%) - LeetHub

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/1910-check-if-binary-string-has-at-most-one-segment-of-ones.py
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/1910-check-if-binary-string-has-at-most-one-segment-of-ones.py
@@ -1,13 +1,6 @@
-class Solution(object):
-    def checkOnesSegment(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def checkOnesSegment(self, s: str) -> bool:
         x = re.sub("0+", " ", s)
-        ans = x.strip().split(" ")
-        print(ans)
-        if len(ans) >=2:
-            return False
-        return True
+        return len(x.strip().split(" ")) < 2
+            
         
