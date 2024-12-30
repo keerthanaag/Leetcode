7ac31bba39a707466ac8c64edde037061b025d0d
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2024-12-30 10:35:21 +0530

    Time: 17 ms (5.48%), Space: 17.7 MB (18.95%) - LeetHub

---
+++ 1689-detect-pattern-of-length-m-repeated-k-or-more-times/1689-detect-pattern-of-length-m-repeated-k-or-more-times.py
@@ -0,0 +1,32 @@
+class Solution:
+    def containsPattern(self, arr: List[int], m: int, k: int) -> bool:
+        for j in range(0,len(arr)-m):
+            word = arr[j:j+m]
+            count = 0
+            for i in range(j,len(arr),m):
+                print(arr[i:i+m])
+                if arr[i:i+m] == word:
+                    count += 1
+                else:
+                    word = arr[i:i+m]
+                    count = 1
+                if count == k:
+                    print("true",word,count)
+                    return True
+        return False
+                    #break
+            #print(count)
+        word = arr[0:m]
+        count = 0
+        for i in range(0,len(arr),m):
+            print(word,arr[i:i+m])
+            if arr[i:i+m] == word:
+                count += 1
+            else:
+                word = arr[i:i+m]
+                count = 1
+            if count == k:
+                return True
+        return False
+        
+        
