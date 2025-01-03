66fa663cb1b3f4f55e864573e4df9858ab1c2171
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2025-01-03 12:04:13 +0530

    Time: 4 ms (7.02%), Space: 17.7 MB (17.73%) - LeetHub

---
+++ 1769-get-maximum-in-generated-array/1769-get-maximum-in-generated-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getMaximumGenerated(self, n: int) -> int:
+        nums = [0] * (n+1) 
+        nums[0] = 0
+        if n > 0:
+            nums[1] = 1
+        for i in range(1,n):
+            flag = 0
+            if (2 <= 2 * i) and (2 * i <= n):
+                nums[2 * i] = nums[i]
+                flag = 1
+            if (2 <= 2 * i + 1) and (2 * i + 1 <= n):
+                nums[2 * i + 1] = nums[i] + nums[i + 1]
+                flag = 1
+            if flag == 0:
+                break
+        return max(nums)
+
+
+        
