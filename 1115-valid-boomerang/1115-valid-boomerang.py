ce4ed5ae0a96e8ae50e1c144c23f199b6399f682
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2025-01-07 09:32:54 +0530

    Time: 0 ms (100%), Space: 17.8 MB (16.14%) - LeetHub

---
+++ 1115-valid-boomerang/1115-valid-boomerang.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isBoomerang(self, points: List[List[int]]) -> bool:
+        x0, y0 = points[0]
+        x1, y1 = points[1]
+        dx, dy = x1 - x0, y1 - y0
+        x, y = points[2]
+        if (y - y0) * dx == (x - x0) * dy:
+            return False
+        return True
+        
