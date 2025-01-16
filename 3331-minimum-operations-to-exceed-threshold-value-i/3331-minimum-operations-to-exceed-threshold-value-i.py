41485105c55628d488ce8fecc3f8e33df925dae8
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2025-01-16 09:52:52 +0530

    Time: 2 ms (21.07%), Space: 17.8 MB (18.51%) - LeetHub

--- 3331-minimum-operations-to-exceed-threshold-value-i/3331-minimum-operations-to-exceed-threshold-value-i.py
+++ 3331-minimum-operations-to-exceed-threshold-value-i/3331-minimum-operations-to-exceed-threshold-value-i.py
@@ -1,20 +1,17 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
         nums.sort()
-        f, l = 0, len(nums)
-        mid = l // 2
-        pos = self.binarySearch(nums, k, f, mid) if nums[mid] >= k else self.binarySearch(nums, k, mid + 1, l)
-        return pos
-
-    def binarySearch(self, nums: List[int], k: int, start: int, end: int) -> int:
-        while start < end:
-            mid = (start + end) // 2
-            if nums[mid] >= k:
-                end = mid
-            else:
-                start = mid + 1
-        return start if start < len(nums) and nums[start] >= k else -1
-
+        f = 0
+        l = len(nums)
+        mid = len(nums)//2
+        if nums[mid] >= k:
+            return self.search(nums,k,f,mid+1)
+        else:
+            return self.search(nums,k,mid+1,l)
+    def search(self,nums: List[int], k: int,start: int,end: int):
+        for i in range(start,end):
+            if nums[i]>= k:
+                    return i
 
             
         
