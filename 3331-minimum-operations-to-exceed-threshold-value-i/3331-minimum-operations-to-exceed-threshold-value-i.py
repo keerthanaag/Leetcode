683a4f5c362389e0e12176c925f7308fb38021f0
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2025-01-16 09:42:14 +0530

    Time: 1 ms (25.86%), Space: 17.7 MB (30.43%) - LeetHub

--- 3331-minimum-operations-to-exceed-threshold-value-i/3331-minimum-operations-to-exceed-threshold-value-i.py
+++ 3331-minimum-operations-to-exceed-threshold-value-i/3331-minimum-operations-to-exceed-threshold-value-i.py
@@ -1,7 +1,18 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
         nums.sort()
-        for i in range(0,len(nums)):
-            if nums[i] >=k:
+        f = 0
+        l = len(nums)
+        mid = len(nums)//2
+        if nums[mid] >= k:
+            pos = self.search(nums,k,f,mid+1)
+        else:
+            pos = self.search(nums,k,mid+1,l)
+        return pos
+    def search(self,nums: List[int], k: int,start: int,end: int):
+        for i in range(start,end):
+            if nums[i]>= k:
                 return i
+
+            
         
