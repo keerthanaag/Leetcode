915bf9e3c77f4bc7a1ff7458f602c2ce212c1d24
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2025-01-19 11:31:49 +0530

    Time: 8 ms (40.61%), Space: 17.8 MB (34.44%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        st = ""
+        for i in range(1,n+1):
+            if i == 1:
+                st +="1"
+            else:
+                temp = st
+                st = ""
+                j = 0
+                while j < len(temp):
+                    if j == 0:
+                        word = temp[j]
+                        count = 1
+                    elif word == temp[j]:
+                        count += 1
+                    else:
+                        st += (str(count)+word)
+                        word = temp[j]
+                        count = 1
+                    j+=1
+                st += (str(count)+word)
+        return st
+
+
+        
