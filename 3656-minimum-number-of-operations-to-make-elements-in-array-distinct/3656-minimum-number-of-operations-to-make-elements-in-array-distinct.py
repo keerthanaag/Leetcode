001bd09c875cf2a8f0c251f549d0676efecfdedc
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2025-01-27 09:50:07 +0530

    Time: 8 ms (35.03%), Space: 12.5 MB (13.17%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -1,10 +1,9 @@
-class Solution:
-    def minimumOperations(self, nums: List[int]) -> int:
+class Solution(object):
+    def minimumOperations(self, nums):
         cnt = 0
         while len(nums) != len(set(nums)):
             if len(nums) == 3:
                 return cnt+1
             nums = nums[3:]
             cnt +=1
-        return cnt
-        
+        return cnt  
