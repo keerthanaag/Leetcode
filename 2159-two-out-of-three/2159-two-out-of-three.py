c83847669f383610cca6fab4e95f02dcb5d79972
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2025-01-28 10:08:45 +0530

    Time: 1 ms (78.36%), Space: 17.8 MB (42.27%) - LeetHub

--- 2159-two-out-of-three/2159-two-out-of-three.py
+++ 2159-two-out-of-three/2159-two-out-of-three.py
@@ -1,5 +1,9 @@
-class Solution(object):
-    def twoOutOfThree(self, nums1, nums2, nums3):
+class Solution:
+    def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
         return list(set(nums1) & set(nums2) | set(nums2) & set(nums3) | set(nums3) & set(nums1))
+        temp = set(nums1).intersection(nums2)
+        temp2 = set(nums2).intersection(nums3)
+        temp3 = set(nums3).intersection(nums1)
+        return list(temp.union(temp2).union(temp3))
 
         
