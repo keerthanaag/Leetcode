30719be16e47e5bea74f6f1796a926e00e9f4064
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2025-01-31 10:40:53 +0530

    Time: 46 ms (80.58%), Space: 18.6 MB (60.38%) - LeetHub

--- 1287-distance-between-bus-stops/1287-distance-between-bus-stops.py
+++ 1287-distance-between-bus-stops/1287-distance-between-bus-stops.py
@@ -1,7 +1,6 @@
-class Solution(object):
-    def distanceBetweenBusStops(self, distance, start, destination):
+class Solution:
+    def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
         min_val = min(start,destination)
         max_val = max(start,destination)
-        print(distance[min_val:max_val],sum(distance[min_val:max_val]))
-        print(distance[max_val:],distance[:min_val],sum(distance[max_val:])+sum(distance[:min_val]))
         return min(sum(distance[min_val:max_val]),sum(distance[max_val:])+sum(distance[:min_val]))
+        
