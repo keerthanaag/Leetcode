f049818ba1d0e4716a9bb40fafd13d1a1daa60f0
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2025-02-01 10:52:10 +0530

    Time: 0 ms (100%), Space: 17.9 MB (22.9%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.py
+++ 3429-special-array-i/3429-special-array-i.py
@@ -2,6 +2,6 @@ class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
         for i in range(len(nums)-1):
             print(nums[i:i+2])
-            if sum(nums[i:i+2]) % 2 == 0:
+            if nums[i] % 2 == nums[i+1] % 2:
                 return False
         return True
