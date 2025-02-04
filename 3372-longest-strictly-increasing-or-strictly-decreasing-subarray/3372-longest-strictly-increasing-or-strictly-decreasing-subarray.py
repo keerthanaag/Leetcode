ac619a81b7aa5c0ea48523ab31a492ab925dfe41
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2025-02-04 09:34:22 +0530

    Time: 10 ms (5.84%), Space: 17.9 MB (34.55%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -3,16 +3,13 @@ class Solution:
         count = 0
         temp = 0
         for i in range(0,len(nums)):
-            #print(temp,nums[i],nums[i-1])
             if i == 0 or nums[i] > nums[i-1]:
                 temp += 1
             else:
                 count = max(count,temp)
                 temp = 1
         count = max(count,temp)
-        print("count: ",count)
         num = nums[::-1]
-        print(num,nums)
         temp = 0
         for i in range(0,len(num)):
             print(temp,num[i],num[i-1])
@@ -21,6 +18,4 @@ class Solution:
             else:
                 count = max(count,temp)
                 temp = 1
-        count = max(count,temp)
-        print("count: ",count)
-        return count
+        return max(count,temp)
