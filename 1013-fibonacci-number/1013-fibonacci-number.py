082f51b0024dfc3dc414e88f1190d2286d308282
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2025-02-07 11:41:58 +0530

    Time: 17 ms (63.16%), Space: 12.3 MB (87.32%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,21 +1,16 @@
-class Solution:
-    def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        else:
-            for i in range(n+1):
-                if i == 0:
-                    prev1 = 0
-                    val = 0
-                elif i == 1:
-                    prev2 = 1
-                    val = 1
-                else:
-                    val = prev1+prev2
-                    prev1 = prev2
-                    prev2 = val
-                    print(prev1,prev2,val)
-            return val
+class Solution(object):
+    def fib(self, n):
+        for i in range(n+1):
+            if i == 0:
+                prev1 = 0
+                val = 0
+            elif i == 1:
+                prev2 = 1
+                val = 1
+            else:
+                val = prev1+prev2
+                prev1 = prev2
+                prev2 = val
+                print(prev1,prev2,val)      
+        return val
         
