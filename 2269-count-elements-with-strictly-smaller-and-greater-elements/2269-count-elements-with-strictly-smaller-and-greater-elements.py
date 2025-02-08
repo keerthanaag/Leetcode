94bfbca8ef5a6e537d656bf7c2254fa35a8aae55
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2025-02-08 09:57:40 +0530

    Time: 1110 ms (5.77%), Space: 12.6 MB (8.65%) - LeetHub

---
+++ 2269-count-elements-with-strictly-smaller-and-greater-elements/2269-count-elements-with-strictly-smaller-and-greater-elements.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def countElements(self, nums):
+        if len(set(nums)) == 1:
+            return 0 
+        elif len(set(nums)) == len(nums):
+            return len(nums)-2
+        else:
+            nums.sort()
+            first = nums[0]
+            last = nums[len(nums)-1]
+            posf =0
+            posl = len(nums)-1
+            print(first,last)
+            for i in range(0,len(nums)):
+                if nums[i] == first:
+                    posf = i
+                if nums[i] != last:
+                    posl = i
+                print(nums,i,nums[i],posf,posl)
+            return posl-posf 
+        
