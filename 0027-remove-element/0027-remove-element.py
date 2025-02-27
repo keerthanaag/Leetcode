05c5b9fa3e9b8a7d3ce22264d78fbf912f7aae84
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2025-02-27 12:53:19 +0530

    Time: 0 ms (100%), Space: 18 MB (11.36%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,14 +1,8 @@
-class Solution(object):
-    def removeElement(self, nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
-        #count the number of element in the list then iterate it the no of times using remove function thn return the length of the list
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
         n=nums.count(val)
         for i in range(0,n):
             nums.remove(val)
         k=len(nums)
         return k
-
+        
