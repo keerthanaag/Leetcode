51e62b5e0a51d66c6843dcb93a67b5f85691e8a4
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2025-03-13 21:44:22 +0530

    Time: 2 ms (27.51%), Space: 17.9 MB (10.03%) - LeetHub

---
+++ 2154-minimum-moves-to-convert-string/2154-minimum-moves-to-convert-string.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimumMoves(self, s: str) -> int:
+        i = 0
+        cnt = 0
+        if len(s) <= 3:
+            if 'X' not in s:
+                return 0
+            return 1
+        while i < len(s):
+            if s[i] == 'O':
+                i += 1
+                continue
+            temp = s[i:i+3]
+            print(i,temp)
+            if 'x' in temp.lower():
+                cnt += 1
+            i += 3
+        return cnt
+
+
+        
