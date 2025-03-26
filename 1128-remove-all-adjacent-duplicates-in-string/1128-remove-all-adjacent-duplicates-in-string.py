d48b749e9cc837ac02f2c173ee28855861258d4f
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2025-03-26 06:29:03 +0530

    Time: 231 ms (5.02%), Space: 18.4 MB (95.75%) - LeetHub

---
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.py
@@ -0,0 +1,19 @@
+from collections import deque
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = deque()
+        for i in range(len(s)):
+            print(i,s[i])
+            if i == 0:
+                stack.append(s[i])
+            else:
+                if len(stack) != 0:
+                    prev = stack.pop()
+                    if prev != s[i]:
+                        stack.append(prev)
+                        stack.append(s[i])
+                else:
+                    stack.append(s[i])
+        print(stack)
+        print("".join(list(stack)))
+        return "".join(list(stack))
