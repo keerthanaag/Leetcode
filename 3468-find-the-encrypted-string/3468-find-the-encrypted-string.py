b92d7fb8a4c272d51256bdc7a4f7a94bfefbcea5
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2025-03-30 10:49:32 +0530

    Time: 4 ms (9.57%), Space: 17.8 MB (70.92%) - LeetHub

--- 3468-find-the-encrypted-string/3468-find-the-encrypted-string.py
+++ 3468-find-the-encrypted-string/3468-find-the-encrypted-string.py
@@ -4,9 +4,7 @@ class Solution:
         temp = []
         while i < len(s):  
             pos = (i+k)  % len(s)
-            print(s[i],s[pos])
             temp.append(s[pos])
             i += 1
-        print(temp)
         return "".join(temp)
                 
