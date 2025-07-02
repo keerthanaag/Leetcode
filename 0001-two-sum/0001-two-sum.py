2a80241e7cbdc77487ced6769bcf4567ea2e8096
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2025-07-03 02:29:16 +0530

    Time: 2047 ms (7.72%), Space: 18.4 MB (78.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,22 +1,6 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-
-        # map = {}
-        # for i in range(len(nums)):
-        #     if nums[i] not in map:
-        #         map[target - nums[i]] = i 
-        #     else:
-        #         return map[nums[i]], i
-
-        # return -1,-1
-        for i in range(len(nums)):
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+       for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if i!=j and target == nums[i]+nums[j]:
                     return [i,j]
-        
-
