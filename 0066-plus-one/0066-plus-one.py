b69136637614425f1ae326f4999f3234474234ae
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2026-01-02 00:04:13 +0530

    Time: 0 ms (100%), Space: 12.3 MB (81.81%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,28 +1,11 @@
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        flag = 0
-        for i in range(len(digits)-1,-1,-1):
-            print(i,digits[i],"first")
-            if flag == 1:
-                if digits[i] < 9:
-                    digits[i] = digits[i]+1
-                    flag = 0
-                    print("break 1")
-                    break
-                else:
-                    digits[i] = 0
-                    flag = 1
-            #print(digits[i])
-            else:
-                if digits[i] < 9 :
-                    digits[i] = digits[i]+1
-                    print("break 2")
-                    break
-                else:
-                    digits[i] = 0
-                    flag = 1
-        if flag == 1:
-            digits.insert(0,1)
-            print(i,digits[i],"last")
-        print(digits)
-        return digits
+class Solution(object):
+    def plusOne(self, digits):
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+
+        return [1] + digits
+
+        
