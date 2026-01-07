1eaff552e417fadd366bf6ab769e3bba85af52a3
Keerthanaa G <95162141+keerthanaag@users.noreply.github.com>
2026-01-07 09:15:48 +0530

    Time: 19 ms (16.79%), Space: 19.6 MB (5.51%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.py
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.py
@@ -7,13 +7,12 @@ class Solution:
             else:
                 temp = i
                 flag = 1
-                #21
-                while i != 0: #
-                    rem = i%10 #1
+                while i != 0: 
+                    rem = i%10 
                     if rem == 0 or temp%rem != 0: 
                         flag = 0
                         break
-                    i = i//10 #2
+                    i = i//10 
                     print(temp,rem,i)
                 if flag == 1:
                     lst.append(temp)
